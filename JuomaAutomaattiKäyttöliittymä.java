de6b35509dc3f4ff4dd6a2f18f5dd32965047aa6
AKajander <43338390+AKajander@users.noreply.github.com>
2018-10-07 20:41:28 +0300

    Add files via upload
    Fix if clauses and structure of gui element

--- JuomaAutomaattiKäyttöliittymä.java
+++ JuomaAutomaattiKäyttöliittymä.java
@@ -6,32 +6,31 @@ public class JuomaAutomaattiKäyttöliittymä {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String vastaus = null;
-		
-		do {
-			vastaus = JOptionPane.showInputDialog(null,
-				"*******Juoma-Automaatti*******\n"+
-				"1. Kahvi\n2.Tee\n3.Kaakao\n4.Lopeta",
-				JOptionPane.QUESTION_MESSAGE);
-			if (vastaus == "1") {
+		do {			
+			String vastaus = JOptionPane.showInputDialog(null, "*******Juoma-Automaatti*******\\n\"+\r\n" + 
+					"\"1. Kahvi\\n2.Tee\\n3.Kaakao\\n4.Lopeta", JOptionPane.QUESTION_MESSAGE);
+			int valinta = Integer.parseInt(vastaus);
+			
+			if (valinta == 1) {
 				JuomaAutomaatti tilattuKahvi = new JuomaAutomaatti();
 				tilattuKahvi.valmistaKahvi();
 				
 				JOptionPane.showMessageDialog(null, tilattuKahvi.toString());
 			}
-			if (vastaus == "2") {
+			if (valinta == 2) {
 				JuomaAutomaatti tilattuTee = new JuomaAutomaatti();
 				tilattuTee.valmistaTee();
 				
 				JOptionPane.showMessageDialog(null, tilattuTee.toString());
 			}
-			if (vastaus == "3") {
+			if (valinta == 3) {
 				JuomaAutomaatti tilattuKaakao = new JuomaAutomaatti();
 				tilattuKaakao.valmistaKaakao();
 				
 				JOptionPane.showMessageDialog(null, tilattuKaakao.toString());
 			}
-		} while (vastaus == "4");
+			// valinta-muuttujaa ei voi käyttää while-ehdossa.. Jäin jumiin, miten saisin toteutettua tämän
+		} while ();
 	}
 
 }
