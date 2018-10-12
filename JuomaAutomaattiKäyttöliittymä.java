85a6b043d4728cb0fdcf39df8b748ecf2c7b5dee
Mika Stenberg <mika.stenberg@laurea.fi>
2018-10-12 13:31:45 +0300

    Korjauksia ja kommentteja
    Katso kommentit ja korjaukset läpi.

--- JuomaAutomaattiKäyttöliittymä.java
+++ JuomaAutomaattiKäyttöliittymä.java
@@ -1,36 +1,51 @@
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
-public class JuomaAutomaattiK�ytt�liittym� {
+public class JuomaAutomaattiKäyttöliittymä {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		do {			
-			String vastaus = JOptionPane.showInputDialog(null, "*******Juoma-Automaatti*******\\n\"+\r\n" + 
-					"\"1. Kahvi\\n2.Tee\\n3.Kaakao\\n4.Lopeta", JOptionPane.QUESTION_MESSAGE);
-			int valinta = Integer.parseInt(vastaus);
-			
+
+		// Alusta muuttujat silmukan ulkopuolella
+		int valinta = 0;
+
+		// Olet luonut jokaisessa kohdassa uuden JuomaAutomaatin. Tämä aiheuttaa sen,
+		// että
+		// Juomat eivät koskaan lopu, sillä uusi automaatti on aina täynnä :)
+		// Eli koko tehtävässä riittää että luodaan yksi automaatti, jota kaikki kohdat
+		// käyttävät
+
+		JuomaAutomaatti matti = new JuomaAutomaatti();
+
+		do {
+			String vastaus = JOptionPane.showInputDialog(null,
+					"*******Juoma-Automaatti*******\\n\"+\r\n" + "\"1. Kahvi\\n2.Tee\\n3.Kaakao\\n4.Lopeta",
+					JOptionPane.QUESTION_MESSAGE);
+			valinta = Integer.parseInt(vastaus);
+
 			if (valinta == 1) {
-				JuomaAutomaatti tilattuKahvi = new JuomaAutomaatti();
-				tilattuKahvi.valmistaKahvi();
-				
-				JOptionPane.showMessageDialog(null, tilattuKahvi.toString());
+
+				matti.valmistaKahvi();
+
+				JOptionPane.showMessageDialog(null, matti.toString());
 			}
 			if (valinta == 2) {
-				JuomaAutomaatti tilattuTee = new JuomaAutomaatti();
-				tilattuTee.valmistaTee();
-				
-				JOptionPane.showMessageDialog(null, tilattuTee.toString());
+				// JuomaAutomaatti matti = new JuomaAutomaatti();
+				matti.valmistaTee();
+
+				JOptionPane.showMessageDialog(null, matti.toString());
 			}
 			if (valinta == 3) {
-				JuomaAutomaatti tilattuKaakao = new JuomaAutomaatti();
-				tilattuKaakao.valmistaKaakao();
-				
-				JOptionPane.showMessageDialog(null, tilattuKaakao.toString());
-			}
-			// valinta-muuttujaa ei voi k�ytt�� while-ehdossa.. J�in jumiin, miten saisin toteutettua t�m�n
-		} while ();
-	}
+				// JuomaAutomaatti matti = new JuomaAutomaatti();
+				matti.valmistaKaakao();
 
+				JOptionPane.showMessageDialog(null, matti.toString());
+			}
+			// valinta-muuttujaa ei voi käyttää while-ehdossa.. Jäin jumiin, miten saisin
+			// toteutettua tämän
+		} while (valinta != 4);
+
+		System.out.println("Ohjelma loppuu.");
+
+	}
 }
